93897ec456a7949e7b23ef83ea4dc95428e7843d
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-14 22:39:03 +0000

    git管理外としたファイルを削除

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import * as express from "express";
-var app = express();
-var port = 3000;
-app.get("/", function (req, res) { return res.send("Hello World!"); });
-app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
-//# sourceMappingURL=index.js.map

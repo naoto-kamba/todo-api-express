c352784581a8bf7e566852b6606e8d023686b635
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-15 02:05:45 +0000

    アプリ開始時にDB接続を作成

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express'
 import 'reflect-metadata'
-const app = express()
-const port = 3000
+import { createConnection } from 'typeorm'
+createConnection().then(async (connection) => {
+  const app = express()
+  const port = 3000
+  app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+})

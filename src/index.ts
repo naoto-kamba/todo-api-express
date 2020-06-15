7a6a081d2256eedbd84179c35edea60977ea0b3f
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-15 04:14:11 +0000

    thenをawaitに変更

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express'
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-createConnection().then(async (connection) => {
+
+const main = async () => {
+  const connection = await createConnection()
+
   const app = express()
   const port = 3000
   app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-})
+}
+
+main()

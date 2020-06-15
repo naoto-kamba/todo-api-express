13c3a75b0a695f0c4448f25eba9de5f130daf401
ayataka0nk <ayataka.nk@gmail.com>
2020-06-15 05:24:24 +0000

    接続テストapi作成

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express'
 import 'reflect-metadata'
-import { createConnection } from 'typeorm'
+import { createConnection, getManager } from 'typeorm'
+import { Task } from './entity/Task'
 
 const main = async () => {
-  const connection = await createConnection()
+  await createConnection()
 
   const app = express()
   const port = 3000
-  app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
+  app.get('/', (req: Request, res: Response) =>
+    res.json({ greeting: 'Hello World!' })
+  )
+  app.get('/connect-test', async (req: Request, res: Response) => {
+    const manager = getManager()
+    const tasks = await manager.find(Task)
+    return res.json(tasks)
+  })
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }

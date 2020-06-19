72a21e63a0c48f422b9571a4e78f119faf2c3bf7
naoto kanba <naoto.kanba@dcom-web.co.jp>
2020-06-19 04:24:24 +0000

    cors設定

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
+import cors, { CorsOptions } from 'cors'
 
 import { route } from './route'
 
@@ -9,6 +10,10 @@ const main = async () => {
 
   const app = express()
   const port = 80
+  const corsOptions: CorsOptions = {
+    origin: ['http://127.0.0.1:8080'],
+  }
+  app.use(cors(corsOptions))
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
   route(app)
